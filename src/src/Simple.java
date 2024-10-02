62d26dcff1e585ffa0bc784ebcdfc2094f78a880
pugit12345 <tinku.sjce@gmail.com>
2024-10-02 13:26:30 +0530

    sec commit

--- src/src/Simple.java
+++ src/src/Simple.java
@@ -2,8 +2,8 @@ import java.util.*;
  class Test1{
     public static void main(String args[]){
         Integer i = 10;
-//        Scanner sc = new Scanner(System.in);
-        String s = " Testin Java" ; // sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
         System.out.println("Hello Java" + s);
 
     }
