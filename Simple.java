33c53d3339b5f62cc2cc0a3d2b3ba5a102777768
pugit12345 <tinku.sjce@gmail.com>
2024-10-02 13:09:00 +0530

    Test Commit

---
+++ Simple.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ class Test1{
+    public static void main(String args[]){
+        Integer i = 10;
+//        Scanner sc = new Scanner(System.in);
+        String s = " Testin Java" ; // sc.nextLine();
+        System.out.println("Hello Java" + s);
+
+    }
+}
+
